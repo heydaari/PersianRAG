59018312760dfb53e6a39f3c4596fa7468662d6b
Mohammad Hasan Heydari <heidary0081@gmail.com>
2025-04-13 15:12:22 +0330

    reorganizing the project

---
+++ src/rag.py
@@ -0,0 +1,8 @@
+from src.retrieve.retrieval import Retrieval
+from src.generation.generation import Generation
+
+class RAG:
+    def __init__(self,
+                 retrieve_model,
+                 generation_model):
+        pass
